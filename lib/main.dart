dca4e863fb01e60ec971369ec43b900fbb643ff9
oussamahame3 <oussamaham75@gmail.com>
2025-04-27 00:56:24 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore: unused_import
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Text("S"),
+    );
+  }
+}
